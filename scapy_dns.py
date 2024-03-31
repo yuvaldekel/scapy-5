b9d140b2c4a7be2dfede757e7f4ec5d497a364c5
Yuval Dekel <yuvddd05@gmail.com>
2024-03-31 10:50:28 +0300

    filter port 53

--- scapy_dns.py
+++ scapy_dns.py
@@ -3,16 +3,21 @@ from scapy.all import *
 FILE = r"C:\Users\yonat\Documents\Yuval\devops\networking\scapy-5\DNS_Q.txt"
 
 def print_query_name(dns_packet):
+    print(dns_packet.show())
     dns_name = dns_packet[DNSQR].qname.decode()
-    print(dns_name)
+    #print(dns_name)
     with open(FILE, 'a') as open_file:
         open_file.write('{}\n'.format(dns_name))
 
 def filter_dns(packet):
-    return (DNS in packet and packet[DNS].opcode == 0 and DNSQR in packet and packet[DNSQR].qtype == 1)
+    return (UDP in packet and packet[UDP].dport == 53 and \
+            DNS in packet and packet[DNS].opcode == 0 and \
+            DNSQR in packet and (packet[DNSQR].qtype == 1 or packet[DNSQR].qtype == 12))
 
 def main():
-    sniff(count=10, lfilter=filter_dns, prn=print_query_name)
-
+#    while True:
+    sniff(count=1, lfilter=filter_dns, prn=print_query_name)
+        
+            
 if __name__ == "__main__":
     main()
